ea691df95bfa205c0365b03e6475e57dc1ffeb5c
Richard Davis <rdavis22@tulane.edu>
2016-09-28 19:44:14 +0100

    Final code for hw4

--- Homeworks/hw4/BIOS_7300_hw4.R
+++ Homeworks/hw4/BIOS_7300_hw4.R
@@ -2,17 +2,20 @@
 #Load libraries and data (Time to death waiting for liver transplant)
 library(survival)
 library(KMsurv)
-#hw4data<-read.table(file=file.choose(), header=T, sep="")
 attach(hw4data)
 
 #Q3####
-agecat<- cut(age, c(0, min(age), 40, 60, max(age)))
+agecat<- cut(age, c(0, 40, 60, max(age)))
+num_age<- as.numeric(agecat)
+age_levels<- cut(num_age, c(0, 1, 2, 3))
 #Part a-interaction model#
-model_1<-coxph(formula = Surv(time, status)~agecat*gender, data=hw4data, method="breslow")
+model_1<-coxph(formula = Surv(time, status)~age_levels*gender, data=hw4data)
+chk_model_1<-cox.zph(model_1) #check assumption of proportional hazards
 #print(model_1)
 
 #Part b-additive model#
-model_2<-coxph(formula = Surv(time, status)~agecat+gender, data=hw4data, method="breslow")
+model_2<-coxph(Surv(time, status)~age_levels+gender, data=hw4data, method="breslow")
 #print(model_2)
 
-#Part c-
+#Part d-variate model#
+model_3<-coxph(Surv(time, status)~num_age+gender, data=hw4data, method="breslow")
